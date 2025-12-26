832d8002f208cb88faa0ad07156cc79702b153d3
Berkem Çınar <cinar.berkem@gmail.com>
2025-12-26 13:58:14 +0000

    - Deleted some old learning projects that were using blueprints too.

--- ObstacleAssault/UnrealLearningKit/Intermediate/Build/Win64/x64/UnrealLearningKitEditor/Development/Core/SharedPCH.Core.Cpp17.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.34.31946
-#include "C:/Repositories/unreal-learning/ObstacleAssault/UnrealLearningKit/Intermediate/Build/Win64/x64/UnrealLearningKitEditor/Development/Core/SharedPCH.Core.Cpp17.h"
--- ObstacleAssault/UnrealLearningKit/Intermediate/Build/Win64/x64/UnrealLearningKitEditor/Development/Engine/SharedPCH.Engine.Cpp20.InclOrderUnreal5_0.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.34.31946
-#include "C:/Repositories/unreal-learning/ObstacleAssault/UnrealLearningKit/Intermediate/Build/Win64/x64/UnrealLearningKitEditor/Development/Engine/SharedPCH.Engine.Cpp20.InclOrderUnreal5_0.h"
